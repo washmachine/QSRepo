a4898afd0f20e192c87b8352bb25f0a18bf64213
Stefan Umfahrer <s.umfahrer@student.tugraz.at>
2022-05-16 10:14:00 +0200

    added propBoolean for triming of gen'ed vals, added R4, R8 and R11

--- qs22_assignment2/Framework/src/test/scala/at/tugraz/ist/qs2022/MessageBoardProperties.scala
+++ qs22_assignment2/Framework/src/test/scala/at/tugraz/ist/qs2022/MessageBoardProperties.scala
@@ -8,6 +8,7 @@ import at.tugraz.ist.qs2022.messageboard.clientmessages._
 import org.junit.runner.RunWith
 import org.scalacheck.Prop.{classify, forAll}
 import org.scalacheck.{Gen, Properties}
+import org.scalacheck.Prop.propBoolean
 
 import scala.jdk.CollectionConverters._
 
@@ -151,9 +152,9 @@ class MessageBoardProperties extends Properties("MessageBoardProperties") {
     
 
     
-    liked_reply_invalid_msg.isInstanceOf[OperationFailed] && liked_reply.isInstanceOf[OperationAck] //&&
-    //disliked_reply_invalid_msg.isInstanceOf[OperationFailed] && disliked_reply.isInstanceOf[OperationAck] &&
-    //reaction_reply_invalid_msg.isInstanceOf[OperationFailed] && reaction_reply.isInstanceOf[OperationAck]
+    liked_reply_invalid_msg.isInstanceOf[OperationFailed] && liked_reply.isInstanceOf[ReactionResponse] &&
+    disliked_reply_invalid_msg.isInstanceOf[OperationFailed] && disliked_reply.isInstanceOf[ReactionResponse] &&
+    reaction_reply_invalid_msg.isInstanceOf[OperationFailed] && reaction_reply.isInstanceOf[ReactionResponse]
   }
 
     /////////DOESNT WORK
@@ -278,6 +279,238 @@ class MessageBoardProperties extends Properties("MessageBoardProperties") {
             }
         }
   }
+
+  property("[R4] A message may only be liked by users who have not yet liked the corresponding message.") =
+    forAll(Gen.alphaStr, Gen.alphaStr, validMessageGen) { (author_msg: String, author_like: String, message: String) =>
+      (author_msg.trim.nonEmpty && author_like.trim.nonEmpty && !author_msg.eq(author_like) && message.trim.nonEmpty) ==> {
+      val sut = new SUTMessageBoard
+      sut.getDispatcher.tell(new InitCommunication(sut.getClient, sut.getCommId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val initAck = sut.getClient.receivedMessages.remove.asInstanceOf[InitAck]
+      val worker: SimulatedActor = initAck.worker
+
+      val msg = new UserMessage(author_msg, message)
+      worker.tell(new Publish(msg, sut.getCommId))
+
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val reply = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new Like(author_like, sut.getCommId, msg.getMessageId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val reply_like = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new Like(author_like, sut.getCommId, msg.getMessageId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val reply_like1 = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new FinishCommunication(sut.getCommId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      sut.getClient.receivedMessages.remove.asInstanceOf[FinishAck]
+
+
+      classify(reply_like != null && reply_like.isInstanceOf[ReactionResponse], "first like valid", "first like invalid") {
+        classify(reply_like1 != null && reply_like1.isInstanceOf[OperationFailed], "second like valid", "second like invalid") {
+            reply_like.isInstanceOf[ReactionResponse] && reply_like1.isInstanceOf[OperationFailed] 
+        }
+      }
+    }
+  }
+
+  property("[R4] A message may only be disliked by users who have not yet disliked the corresponding message.") =
+    forAll(Gen.alphaStr, Gen.alphaStr, validMessageGen) { (author_msg: String, author_dislike: String, message: String) =>
+      (author_msg.trim.nonEmpty && author_dislike.trim.nonEmpty && !author_msg.eq(author_dislike) && message.trim.nonEmpty) ==> {
+      val sut = new SUTMessageBoard
+      sut.getDispatcher.tell(new InitCommunication(sut.getClient, sut.getCommId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val initAck = sut.getClient.receivedMessages.remove.asInstanceOf[InitAck]
+      val worker: SimulatedActor = initAck.worker
+
+      val msg = new UserMessage(author_msg, message)
+      worker.tell(new Publish(msg, sut.getCommId))
+
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val reply = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new Dislike(author_dislike, sut.getCommId, msg.getMessageId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val reply_dislike = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new Dislike(author_dislike, sut.getCommId, msg.getMessageId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val reply_dislike1 = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new FinishCommunication(sut.getCommId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      sut.getClient.receivedMessages.remove.asInstanceOf[FinishAck]
+
+
+      classify(reply_dislike != null && reply_dislike.isInstanceOf[ReactionResponse], "first dislike valid", "first dislike invalid") {
+        classify(reply_dislike1 != null && reply_dislike1.isInstanceOf[OperationFailed], "second dislike valid", "second dislike invalid") {
+            reply_dislike.isInstanceOf[ReactionResponse] && reply_dislike1.isInstanceOf[OperationFailed] 
+        }
+      }
+    }
+  }
+
+  property("[R8] If a user has been reported at least USER BLOCKED AT COUNT (= 6) times, he/she cannot send any further Publish, Like, Dislike or Report messages.") =
+    forAll(Gen.listOfN(100, Gen.alphaStr).map(_.distinct.take(7)), validMessageGen, validMessageGen) { (authors: List[String], message: String, message0: String) =>
+      (authors.forall(x => x.trim.nonEmpty) && message.trim.nonEmpty && message0.trim.nonEmpty) ==> {
+      val sut = new SUTMessageBoard
+      sut.getDispatcher.tell(new InitCommunication(sut.getClient, sut.getCommId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val initAck = sut.getClient.receivedMessages.remove.asInstanceOf[InitAck]
+      val worker: SimulatedActor = initAck.worker
+
+      
+      val msg = new UserMessage(authors(6), message0)
+      //not banned yet
+      worker.tell(new Publish(msg, sut.getCommId))
+      while (sut.getClient.receivedMessages.isEmpty)
+          sut.getSystem.runFor(1)
+      val reply = sut.getClient.receivedMessages.remove()
+
+      
+      //bann 
+      val author_banned = authors(6)
+      for (i <- 0 until USER_BLOCKED_AT_COUNT) {
+        worker.tell(new Report(authors(i), sut.getCommId, author_banned))
+        while (sut.getClient.receivedMessages.isEmpty)
+            sut.getSystem.runFor(1)
+        val replyReport = sut.getClient.receivedMessages.remove()
+        assert(replyReport.isInstanceOf[OperationAck]) // if assert hits -> Gen of 7 unique authors failed
+      }
+
+      worker.tell(new Publish(new UserMessage(author_banned, message), sut.getCommId))
+      while (sut.getClient.receivedMessages.isEmpty)
+          sut.getSystem.runFor(1)
+      val reply_publish = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new Like(author_banned, sut.getCommId, msg.getMessageId))
+      while (sut.getClient.receivedMessages.isEmpty)
+          sut.getSystem.runFor(1)
+      val reply_like = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new Dislike(author_banned, sut.getCommId, msg.getMessageId))
+      while (sut.getClient.receivedMessages.isEmpty)
+          sut.getSystem.runFor(1)
+      val reply_dislike = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new Report(author_banned, sut.getCommId, authors(0)))
+      while (sut.getClient.receivedMessages.isEmpty)
+          sut.getSystem.runFor(1)
+      val reply_report = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new FinishCommunication(sut.getCommId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      sut.getClient.receivedMessages.remove.asInstanceOf[FinishAck]
+
+      classify(reply_publish.isInstanceOf[UserBanned], "success: banned author couldnt Publish", "failure: banned author was able to Publish") {
+        classify(reply_like.isInstanceOf[UserBanned], "success: banned author couldnt Like", "failure: banned author was able to Like") {
+          classify(reply_dislike.isInstanceOf[UserBanned], "success: banned author couldnt Dislike", "failure: banned author was able to Dislike") {
+            classify(reply_report.isInstanceOf[UserBanned], "success: banned author couldnt Report", "failure: banned author was able to Report") {
+              reply_publish.isInstanceOf[UserBanned] && reply_like.isInstanceOf[UserBanned] && reply_dislike.isInstanceOf[UserBanned] && reply_report.isInstanceOf[UserBanned]
+            }
+          }
+        }
+      }
+    }
+  }
+
+  
+  
+  property("[R11] If a message has been liked, two points should be added to the messages points counter. If a message has been disliked, one point should be removed.") =
+    forAll(Gen.alphaStr, Gen.alphaStr, validMessageGen) { (author_msg: String, author_dislike: String, message: String) =>
+      (author_msg.trim.nonEmpty && author_dislike.trim.nonEmpty && !author_msg.eq(author_dislike) && message.trim.nonEmpty) ==> {
+      val sut = new SUTMessageBoard
+      sut.getDispatcher.tell(new InitCommunication(sut.getClient, sut.getCommId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val initAck = sut.getClient.receivedMessages.remove.asInstanceOf[InitAck]
+      val worker: SimulatedActor = initAck.worker
+
+      val msg = new UserMessage(author_msg, message)
+      worker.tell(new Publish(msg, sut.getCommId))
+
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      val reply = sut.getClient.receivedMessages.remove()
+
+      worker.tell(new Dislike(author_dislike, sut.getCommId, msg.getMessageId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+
+      val reply_dislike = sut.getClient.receivedMessages.remove().asInstanceOf[ReactionResponse]
+      val reply_dislike_points = reply_dislike.points
+
+      worker.tell(new Like(author_msg, sut.getCommId, msg.getMessageId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+
+      val reply_like = sut.getClient.receivedMessages.remove().asInstanceOf[ReactionResponse]
+      val reply_like_points = reply_like.points
+
+      worker.tell(new FinishCommunication(sut.getCommId))
+      while (sut.getClient.receivedMessages.isEmpty)
+        sut.getSystem.runFor(1)
+      sut.getClient.receivedMessages.remove.asInstanceOf[FinishAck]
+
+      //points default is 0
+      classify(reply_dislike.isInstanceOf[ReactionResponse] && reply_dislike_points == -1  , "dislike successfully reduced points by one", "dislike unsuccessful"){
+        classify(reply_like.isInstanceOf[ReactionResponse] && reply_like_points == 1 ,  "like successfully increased points by two", "like unsuccessful") {
+            reply_like_points == 1 && reply_dislike_points == -1
+        }
+      }
+    }
+  }
+  //property("[R13] A dislike may only be deleted if message was disliked before.") =
+  //  forAll(Gen.alphaStr, Gen.alphaStr, validMessageGen) { (author_msg: String, author_dislike: String, message: String) =>
+  //    (author_msg.trim.nonEmpty && author_dislike.trim.nonEmpty && !author_msg.eq(author_dislike) && message.trim.nonEmpty) ==> {
+  //    val sut = new SUTMessageBoard
+  //    sut.getDispatcher.tell(new InitCommunication(sut.getClient, sut.getCommId))
+  //    while (sut.getClient.receivedMessages.isEmpty)
+  //      sut.getSystem.runFor(1)
+  //    val initAck = sut.getClient.receivedMessages.remove.asInstanceOf[InitAck]
+  //    val worker: SimulatedActor = initAck.worker
+//
+  //    val msg = new UserMessage(author_msg, message)
+  //    worker.tell(new Publish(msg, sut.getCommId))
+//
+  //    while (sut.getClient.receivedMessages.isEmpty)
+  //      sut.getSystem.runFor(1)
+  //    val reply = sut.getClient.receivedMessages.remove()
+//
+  //    worker.tell(new Dislike(author_dislike, sut.getCommId, msg.getMessageId))
+  //    while (sut.getClient.receivedMessages.isEmpty)
+  //      sut.getSystem.runFor(1)
+  //    val reply_dislike = sut.getClient.receivedMessages.remove()
+//
+//
+//
+  //    worker.tell(new FinishCommunication(sut.getCommId))
+  //    while (sut.getClient.receivedMessages.isEmpty)
+  //      sut.getSystem.runFor(1)
+  //    sut.getClient.receivedMessages.remove.asInstanceOf[FinishAck]
+//
+//
+  //    classify(reply_dislike != null && reply_dislike.isInstanceOf[ReactionResponse], "first dislike valid", "first dislike invalid") {
+  //      classify(reply_dislike1 != null && reply_dislike1.isInstanceOf[OperationFailed], "second dislike valid", "second dislike invalid") {
+  //          reply_dislike.isInstanceOf[ReactionResponse] && reply_dislike1.isInstanceOf[OperationFailed] 
+  //      }
+  //    }
+  //  }
+  //}
 }
 
 
